a4b7f2daea7bf6575c8e3ab21bfba641f34a5c29
AndrewDuquet <23025602-AndrewDuquet@users.noreply.replit.com>
2025-01-27 23:12:27 +0000

    Agent query: Can you now shoot multiple bullets continuously and hear the sound effects? Does the game continue until game over?
    Refactor audio management to create sounds on demand, improving efficiency and removing pre-creation.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/e91112cc-ef0c-4694-917d-52a886058cf9/8e0b8f3a-9d2f-452c-984d-5d122bd36cda.jpg

--- static/js/audio.js
+++ static/js/audio.js
@@ -1,40 +1,42 @@
 class AudioManager {
     constructor() {
         this.context = new (window.AudioContext || window.webkitAudioContext)();
-        this.sounds = {};
-        this.initialize();
-    }
-
-    async initialize() {
-        await this.createSound('shoot', [0, 0.1, 0.2, 0], 0.1, 'square');
-        await this.createSound('explosion', [0, 0.1, 0.2, 0], 0.3, 'sawtooth');
-        await this.createSound('playerDeath', [0, 0.2, 0.3, 0], 0.4, 'triangle');
-    }
-
-    async createSound(name, envelope, duration, type) {
-        const oscillator = this.context.createOscillator();
-        const gainNode = this.context.createGain();
-        
-        oscillator.type = type;
-        oscillator.connect(gainNode);
-        gainNode.connect(this.context.destination);
-        
-        const startTime = this.context.currentTime;
-        envelope.forEach((value, index) => {
-            gainNode.gain.setValueAtTime(value, startTime + (duration * (index / (envelope.length - 1))));
-        });
-        
-        this.sounds[name] = {
-            play: () => {
-                oscillator.start(startTime);
-                oscillator.stop(startTime + duration);
-            }
+        this.soundParameters = {
+            'shoot': { envelope: [0, 0.1, 0.2, 0], duration: 0.1, type: 'square' },
+            'explosion': { envelope: [0, 0.1, 0.2, 0], duration: 0.3, type: 'sawtooth' },
+            'playerDeath': { envelope: [0, 0.2, 0.3, 0], duration: 0.4, type: 'triangle' }
         };
     }
 
+    async initialize() {
+        // No need to pre-create sounds, we'll create them on demand
+    }
+
+    createSound(type) {
+        const params = this.soundParameters[type];
+        const oscillator = this.context.createOscillator();
+        const gainNode = this.context.createGain();
+
+        oscillator.type = params.type;
+        oscillator.connect(gainNode);
+        gainNode.connect(this.context.destination);
+
+        const startTime = this.context.currentTime;
+        params.envelope.forEach((value, index) => {
+            gainNode.gain.setValueAtTime(
+                value,
+                startTime + (params.duration * (index / (params.envelope.length - 1)))
+            );
+        });
+
+        return { oscillator, startTime, duration: params.duration };
+    }
+
     playSound(name) {
-        if (this.sounds[name]) {
-            this.sounds[name].play();
+        if (this.soundParameters[name]) {
+            const sound = this.createSound(name);
+            sound.oscillator.start(sound.startTime);
+            sound.oscillator.stop(sound.startTime + sound.duration);
         }
     }
 }
